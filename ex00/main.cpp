bf7670b361b24a31896d9328e861e5c886044bea
= <nicostrong@msn.com>
2025-03-11 11:15:33 +0100

    "Update main of test"

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,34 +6,76 @@
 /*   By: nfordoxc <nfordoxc@42luxembourg.lu>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 17:58:10 by nfordoxc          #+#    #+#             */
-/*   Updated: 2025/02/21 07:59:44 by nfordoxc         ###   Luxembourg.lu     */
+/*   Updated: 2025/03/11 11:14:46 by nfordoxc         ###   Luxembourg.lu     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-int	main( void )
+void	testBureaucrat( const std::string &name, int grade )
 {
 	try
 	{
-		Bureaucrat b1("Alice", 151); // Déclenche une exception
-	}
-	catch (std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+		Bureaucrat	bureaucrat(name, grade);
+		std::cout << bureaucrat << std::endl;
 
-	try
-	{
-		Bureaucrat b2("Bob", 1);
-		std::cout << b2 << std::endl;
-		std::cout << "we increase the grade of Bob (1 -> 0 => NOT POSSIBLE)" << std::endl;
-		b2.incGrade(); // Déclenche une exception
+		std::cout << "Try to increase the grade ..." << std::endl;
+		bureaucrat.incGrade();
+		std::cout << "After incrementation: " << bureaucrat << std::endl;
+
+		std::cout << "Try to decrease the grade .." << std::endl;
+		bureaucrat.decGrade();
+		std::cout << "After decrease: " << bureaucrat << std::endl;
+		
+		std::cout << "Try to decrease the grade .." << std::endl;
+		bureaucrat.decGrade();
+		std::cout << "After decrease: " << bureaucrat << std::endl;
+
 	}
-	catch (std::exception &e)
+	catch (const std::exception &e)
 	{
-		std::cerr << e.what() << std::endl;
+		std::cout << e.what() << std::endl;
 	}
+}
+
+int		main( void )
+{
+	std::cout << "\t\t\t=== TEST BUREAUCRAT ===" << std::endl;
+
+	std::cout	<< GREEN
+				<< "\n\t\t--- Test Bureaucrat valid (Grade 75) ---"
+				<< RESET
+				<< std::endl
+				<< std::endl;
+	testBureaucrat("Alice", 75);
+
+	std::cout	<< BLUE
+				<< "\n\t\t--- Test Grade too hight (Grade 0) ---"
+				<< RESET
+				<< std::endl
+				<< std::endl;
+	testBureaucrat("Bob", 0);
+
+	std::cout	<< BLUE
+				<< "\n\t\t--- Test Grade too low (Grade 151) ---"
+				<< RESET
+				<< std::endl
+				<< std::endl;
+	testBureaucrat("Charlie", 151);
+
+	std::cout	<< YELLOW
+				<< "\n\t\t--- Test invalid decrease ---"
+				<< RESET
+				<< std::endl
+				<< std::endl;
+	testBureaucrat("Edward", 150);
+
+	std::cout	<< YELLOW
+				<< "\n\t\t--- Test invalid increase ---"
+				<< RESET
+				<< std::endl
+				<< std::endl;
+	testBureaucrat("Paul", 1);
 
 	return (0);
 }
